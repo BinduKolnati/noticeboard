40ae53c7bddb86e8d275849146a54dc82f0a407d
Bindu Kolnati <khimabindu555@gmail.com>
2020-08-30 22:15:33 +0530

    add all files

---
+++ about.php
@@ -0,0 +1,3 @@
+<h2 style="background-color:#99b3ff;"><b>About</b></h2>
+<p> An online notice board is a place where people can leave any type of messages and notifications, for example, to advertise things, announce events or provide any information.
+</p>
---
+++ contact.php
@@ -0,0 +1,49 @@
+<h3 style="background-color:#99b3ff;"><b>Quick Contact</b></h3>
+<form method="post">
+	<div class="row">
+		<div class="col-md-8">
+		<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4"><?php echo @$err;?></div>
+	</div>
+	
+	
+	
+	<div class="row">
+		<div class="col-sm-4">Enter Your Email</div>
+		<div class="col-sm-5">
+		<input type="email" name="e" class="form-control"/></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-4">Enter Your Mobile</div>
+		<div class="col-sm-5">
+		<input type="password" name="p" class="form-control"/></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-4">Enter Your Message</div>
+		<div class="col-sm-5">
+		<input type="password" name="p" class="form-control"/></div>
+	</div>
+	
+	<div class="row" style="margin-top:10px">
+		<div class="col-sm-2"></div>
+		<div class="col-sm-8">
+		<input type="submit" value="Send Query" name="save" class="btn btn-success"/>
+		
+		</div>
+	</div>
+		
+		</div>
+		<div class="col-md-4">
+		<h3 ><b>Contact Us</b></h3>
+		
+		Name: Bindu Kolnati<br/>
+		Mobile: 9008989090<br/>
+		Email:abcdef@gmail.com<br/>
+		</div>
+	</div>
+	
+	
+</form>	
